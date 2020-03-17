3864426f7f76856e37f330a14d2b14191400d72d
benjamorgan <bamorgan@miuandes.cl>
2020-03-17 16:19:10 -0300

    laboratorio 1 primer commit

---
+++ laboratorio_1_benjamorgan/laboratorio_1_benjamorgan/Persona.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laboratorio_1_benjamorgan
+{
+    class Persona
+    {
+        public string name;
+        public string lastname;
+
+
+        public Persona(string Aname ,string Lastname)
+        {
+            name = Aname;
+            lastname = Lastname;
+        }
+
+        public int RandomNumber()
+        {
+            Random random = new Random();
+            return random.Next(0, 3);
+        }
+
+
+
+
+        public int lanzar()
+        {
+            return    RandomNumber();   
+        }
+    }
+
+
+
+
+
+    }
+
---
+++ laboratorio_1_benjamorgan/laboratorio_1_benjamorgan/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laboratorio_1_benjamorgan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Persona persona1 = new Persona("Bob", "Kunga");
+            Console.WriteLine(persona1.lanzar());
+            Console.ReadLine();
+        }
+    }
+}
